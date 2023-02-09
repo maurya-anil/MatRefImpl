8f2fdc0bd64c68b9272e49a85b6930e9d4df9b0b
Anil Maurya <maurya.anil@gmail.com>
2023-02-09 19:06:58 +0530

    client and about forms added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ClientFormComponent} from './client-form/client-form.component';
+import {AboutComponent} from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {    path:'', component:ClientFormComponent
+  },
+  {
+    path:'client', component:ClientFormComponent
+  },
+  {
+    path:'about', component:AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopnavComponent } from './topnav/topnav.component';
+import { ClientFormComponent } from './client-form/client-form.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopnavComponent,
+    ClientFormComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
