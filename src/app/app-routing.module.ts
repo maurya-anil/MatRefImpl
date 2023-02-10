f06047e16052614710601b08feed9c0eaaa5f3d6
Anil Maurya <maurya.anil@gmail.com>
2023-02-10 11:21:52 +0530

    CSS Grid sample added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ClientFormComponent} from './client-form/client-form.component';
 import {AboutComponent} from './about/about.component';
+import {AddressComponent} from './address/address.component';
 
 const routes: Routes = [
   {    path:'', component:ClientFormComponent
   },
   {
     path:'client', component:ClientFormComponent
+  },
+  {
+    path:'address', component:AddressComponent
   },
   {
     path:'about', component:AboutComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,20 +14,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TopnavComponent } from './topnav/topnav.component';
 import { ClientFormComponent } from './client-form/client-form.component';
 import { AboutComponent } from './about/about.component';
+import { AddressComponent } from './address/address.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopnavComponent,
     ClientFormComponent,
-    AboutComponent
+    AboutComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatDividerModule
     
   ],
   providers: [],
