973d8171e49a29bf35b461c29e78ebeb5b18ce4c
Anil Maurya <maurya.anil@gmail.com>
2023-02-18 14:40:01 +0530

    tabular data component added

--- src/app/components/tabular-data/tabular-data.component.ts
+++ src/app/components/tabular-data/tabular-data.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-tabular-data',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./tabular-data.component.css']
 })
 export class TabularDataComponent {
+  @Input() tableDef:TableDef = {columns:[]};  // Columns and any table level options can be provided
+ // @Input() columns: Array<TableColumn> = [];
+  @Input() dataset: Array<any> = [];
 
+  displayedColumns: string[] = [];
+  
+  dataSource: MatTableDataSource<any> = new MatTableDataSource<any>();
+
+  highlightedRow:any = null;
+
+
+  ngOnInit() {
+
+    // set table columns
+    this.displayedColumns = this.displayedColumns.concat(this.tableDef.columns.map(x => x.columnDef));    // pre-fix static
+
+    this.dataSource = new MatTableDataSource(this.dataset);
+  }
+  onClick(row:any)
+  {
+      console.log ("row clicked"+JSON.stringify(row));
+      if (row == this.highlightedRow) // already selected
+          this.highlightedRow = null;
+      else
+          this.highlightedRow = row; // tbis row gets selected
+  }
+}
+
+export interface TableColumn
+{
+   columnDef:string;
+   header:string
+}
+export interface TableDef
+{
+  columns:Array<TableColumn>;
 }
--- src/app/tabular-data-host/tabular-data-host.component.ts
+++ src/app/tabular-data-host/tabular-data-host.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TabularDataComponent, TableDef} from '../components/tabular-data/tabular-data.component';
 
 @Component({
   selector: 'app-tabular-data-host',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./tabular-data-host.component.css']
 })
 export class TabularDataHostComponent {
+  dataSource:any = null;
+  columntitles:string[] = ['Position', 'Name', 'Weight', 'Symbol'];
+  columnNames:string[] = ['position', 'name', 'weight', 'symbol'];
+  columns = [
+    
+    { columnDef: 'position', header: 'Position' },
+    { columnDef: 'name', header: 'Name' },
+    { columnDef: 'weight', header: 'Weight' },
+    { columnDef: 'symbol', header: 'Symbol' },
+  ]
+  ELEMENT_DATA = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  ];
+  
+  tableDef:TableDef = {columns:this.columns};
+  
+  ngOnInit()
+  {
+      var data:any = [];
+   
+      let obj:any = {};
+      for (var element of this.ELEMENT_DATA)
+      {
+        obj[this.columnNames[0]] = element.position;
+        obj[this.columnNames[1]] = element.name;
+        obj[this.columnNames[2]] = element.weight;
+        obj[this.columnNames[3]] = element.symbol;
+        data.push (obj);
+        obj = {};
+      }
+      this.dataSource = data;  
+      console.log (data);
+  }
 
 }
+
